c83fc801466d7f1c5c6c5a041a9b66072815cf6b
TNXG <tnxg@outlook.jp>
2025-07-10 22:57:32 +0800

    add merge

--- merge.py
+++ merge.py
@@ -2,6 +2,7 @@ import os
 import platform
 import subprocess
 
+
 # 根据操作系统获取 git 仓库的 URL
 def get_git_url(user_repo):
     os_name = platform.system()
@@ -13,25 +14,31 @@ def get_git_url(user_repo):
         # 否则，使用 SSH URL
         return "git@github.com:" + user_repo + ".git"
 
+
 # 运行 git 命令并返回输出
 def run_git_command(command):
     try:
-        command_str = ' '.join(command)
-        result = subprocess.run(command_str, check=True, capture_output=True, text=True, shell=True)
-        print(command_str,result.stdout)  # 打印命令的输出
+        command_str = " ".join(command)
+        result = subprocess.run(
+            command_str, check=True, capture_output=True, text=True, shell=True
+        )
+        print(command_str, result.stdout)  # 打印命令的输出
         return result.stdout
     except subprocess.CalledProcessError as e:
         print(f"Git 命令执行失败: {' '.join(command)}")
         print(f"输出: {e.output}")
         return None
 
+
 def get_remote_name(user_repo):
-    return user_repo.replace("/", "-")    
+    return user_repo.replace("/", "-")
+
 
 # 检查远程仓库是否存在
 def remote_exists(remote_name):
     remotes = run_git_command(["git", "remote"])
     return remote_name in remotes.splitlines()
+
 
 # 将 user_repo 的代码合并到当前仓库
 def merge_with_log(user_repo):
@@ -53,7 +60,7 @@ def merge_with_log(user_repo):
         print(f"获取 {remote_name} 的数据失败。")
         return
     # 尝试合并远程的 main 分支
-    merge_command = ["git", "merge", remote_name + "/main"]
+    merge_command = ["git", "merge", remote_name + "/master"]
     # 尝试合并
     merge_output = run_git_command(merge_command)
     if merge_output is None:
@@ -63,5 +70,6 @@ def merge_with_log(user_repo):
         # 如果合并成功，输出合并的输出
         print("合并成功。输出:")
         print(merge_output)
-        
+
+
 merge_with_log("mx-space/core")
